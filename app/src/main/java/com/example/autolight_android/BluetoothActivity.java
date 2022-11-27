ef1bb59a9a70d6c7ac72a0f225297d6dabf21b6d
kmj_1027@naver.com <mj00102754@gmail.com>
2022-11-27 15:48:40 +0900

    sendSuccess

--- app/src/main/java/com/example/autolight_android/BluetoothActivity.java
+++ app/src/main/java/com/example/autolight_android/BluetoothActivity.java
@@ -7,6 +7,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -15,6 +16,8 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,6 +27,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
@@ -32,7 +37,6 @@ import java.util.UUID;
 public class BluetoothActivity extends AppCompatActivity {
 
     private final String TAG = MainActivity.class.getSimpleName();
-    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
 
     // 블루투스 변수
     private final static int REQUEST_ENABLE_BT = 1;
@@ -47,7 +51,6 @@ public class BluetoothActivity extends AppCompatActivity {
     ImageButton search;
     ListView list;
 
-    private ConnectedThread connectedThread;
     BluetoothSocket btSocket = null;
     boolean flag;
 
@@ -100,7 +103,8 @@ public class BluetoothActivity extends AppCompatActivity {
         // checkSelfPermission을 해주어야 밑에 enableBtIntent 사용 가능
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (btAdapter == null) { // 블루투스 지원 X
             Toast.makeText(getApplicationContext(), "블루투스를 지원하지 않는 기기입니다.\n앱 이용이 불가합니다.", Toast.LENGTH_LONG).show();
@@ -116,6 +120,8 @@ public class BluetoothActivity extends AppCompatActivity {
     public void pairedDevice() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        if(btAdapter.isDiscovering()){
+            btAdapter.cancelDiscovery();}
         btArrayAdapter.clear();
         if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
         pairedDevices = btAdapter.getBondedDevices();
@@ -161,9 +167,11 @@ public class BluetoothActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
                 String deviceName = device.getName();
                 String deviceHardwareAddress = device.getAddress(); // MAC address
-                btArrayAdapter.add(deviceName);
-                deviceAddressArray.add(deviceHardwareAddress);
-                btArrayAdapter.notifyDataSetChanged();
+                if(deviceName != null && deviceHardwareAddress !=null && !deviceAddressArray.contains(deviceHardwareAddress)) {
+                    btArrayAdapter.add(deviceName);
+                    deviceAddressArray.add(deviceHardwareAddress);
+                    btArrayAdapter.notifyDataSetChanged();
+                }
             }
         }
     };
@@ -179,50 +187,98 @@ public class BluetoothActivity extends AppCompatActivity {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
-
             final String name = btArrayAdapter.getItem(position); // get name
             final String address = deviceAddressArray.get(position); // get address
-
-            new Thread() {
-                public void run(){
-                    flag = true;
-                    BluetoothDevice device = btAdapter.getRemoteDevice(address);
-                    // create & connect socket
-                    try {
-                        if (ActivityCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                            ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-                        }
-                        btSocket = createBluetoothSocket(device);
-                        btSocket.connect();
-                    } catch (IOException e) {
-                        try {
-                            flag = false;
-                            btSocket.close();
-                        } catch (IOException e2) {
-                            Toast.makeText(getApplicationContext(), "연결에 실패하였습니다.", Toast.LENGTH_LONG).show();
-                            e.printStackTrace();
-                        }
-                    }
-                    if(flag){
-                        connectedThread = new ConnectedThread(btSocket);
-                        connectedThread.start();
-                        Toast.makeText(getApplicationContext(), name + " 연결에 성공하였습니다.", Toast.LENGTH_LONG).show();
-                    }
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+            // create & connect socket
+            try {
+                if (ActivityCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
                 }
-            }.start();
+                UUID deviceUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB".toString());
+                btSocket = device.createRfcommSocketToServiceRecord(deviceUUID);
+                Log.d("Where","1");
+                btSocket.connect();
+                Log.d("Where","2");
+                ConnectedThread btThread = new ConnectedThread(btSocket);
+                Log.d("Where","3");
+                btThread.start();
+                Log.d("Where","4");
+                Toast.makeText(getApplicationContext(), name + " 연결에 성공하였습니다.", Toast.LENGTH_LONG).show();
+                Log.d("Where","5");
+                btThread.write("HELLO");
+            } catch (IOException e) {
+                try{
+                    btSocket.close();
+                    Toast.makeText(getApplicationContext(), name + " 연결에 실패하였습니다.", Toast.LENGTH_LONG).show();
+                }catch(IOException e2){
+                    e2.printStackTrace();
+                }
+                e.printStackTrace();
+            }
         }
     }
+    public class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
 
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        if (ActivityCompat.checkSelfPermission(BluetoothActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(BluetoothActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
         }
-        try {
-            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
-            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
-        } catch (Exception e) {
-            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.available();
+                    if (bytes != 0) {
+                        buffer = new byte[1024];
+                        SystemClock.sleep(100); //pause and wait for rest of data. Adjust this depending on your sending speed.
+                        bytes = mmInStream.available(); // how many bytes are ready to be read?
+                        bytes = mmInStream.read(buffer, 0, bytes); // record how many bytes we actually read
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
         }
-        return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String input) {
+            byte[] bytes = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
