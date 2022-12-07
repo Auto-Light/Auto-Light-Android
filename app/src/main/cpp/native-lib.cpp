8209ba6ccb124a511f27aa310a4193267612b41e
kgh128 <hayoung0627@gmail.com>
2022-12-07 21:18:13 +0900

    BUG: 얼굴 추출한 Mat이 밝기 추출로 넘어갈 때 제대로 넘어가지 않음.
    - 얼굴 추출해서 얼굴에 맞게 Mat을 잘랐는데, 실제로 밝기 추출로 넘어가는 범위 카메라 프레임 범위로 넘어가서 얼굴 밝기가 추출되지 않고, 프레임 전체가 추출되는 문제점을 해결하였다.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -101,13 +101,13 @@ Java_com_example_autolight_1android_control_1light_ControlLightActivity_getFacel
                          real_facesize_y + real_facesize_height / 2);
             ellipse(img_result, center, Size(real_facesize_width / 2, real_facesize_height / 2), 0,
                     0, 360,
-                    Scalar(255, 0, 255), 30, 8, 0);
+                    Scalar(255, 0, 255), 3, 8, 0);
 
 
             Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,
                            real_facesize_height);
-            rectangle(img_input, Rect(real_facesize_x, real_facesize_y, real_facesize_width,
-                                      real_facesize_height), Scalar(0, 0, 255), 2, 8, 0);
+            //rectangle(img_input, Rect(real_facesize_x, real_facesize_y, real_facesize_width,
+                                     // real_facesize_height), Scalar(0, 0, 255), 2, 8, 0);
             Mat faceROI = img_gray(face_area);
 
             int meanOutput;
@@ -118,55 +118,6 @@ Java_com_example_autolight_1android_control_1light_ControlLightActivity_getFacel
     }
     else //추출안되었을때
         return -1;
-    // TODO: implement getFacelight()
-}
-extern "C"
-JNIEXPORT jint JNICALL
-Java_com_example_autolight_1android_customize_1standard_CustomizeStandardActivity_getFacelight(
-        JNIEnv *env, jobject thiz, jlong cascade_classfier_face, jlong mat_addr_input,
-        jlong mat_addr_result) {
-
-    Mat &img_input = *(Mat *) mat_addr_input;
-    Mat &img_result = *(Mat *) mat_addr_result;
-
-    img_result = img_input.clone();
-
-    std::vector<Rect> faces;
-    Mat img_gray;
-
-    cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
-    equalizeHist(img_gray, img_gray);
-
-    Mat img_resize;
-    float resizeRatio = resize(img_gray, img_resize, 640);
-
-    //-- Detect faces
-    ((CascadeClassifier *) cascade_classfier_face)->CascadeClassifier::detectMultiScale(img_resize,
-                                                                                        faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-
-    //__android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ",
-    //                  (char *) "face %d found ", faces.size());
-
-    for (int i = 0; i < faces.size(); i++) {
-        double real_facesize_x = faces[i].x / resizeRatio;
-        double real_facesize_y = faces[i].y / resizeRatio;
-        double real_facesize_width = faces[i].width / resizeRatio;
-        double real_facesize_height = faces[i].height / resizeRatio;
-
-        Point center( real_facesize_x + real_facesize_width / 2, real_facesize_y + real_facesize_height/2);
-        ellipse(img_result, center, Size( real_facesize_width / 2, real_facesize_height / 2), 0, 0, 360,
-                Scalar(255, 0, 255), 30, 8, 0);
-
-
-        Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,real_facesize_height);
-        rectangle(img_input, Rect(real_facesize_x,real_facesize_y,real_facesize_width,real_facesize_height), Scalar(0, 0, 255), 1, 8, 0);
-        Mat faceROI = img_gray( face_area );
-
-        int meanOutput;
-        meanOutput = (int)mean(faceROI)[0];
-        return meanOutput;
-    }
     // TODO: implement getFacelight()
 }
 extern "C"
@@ -230,19 +181,21 @@ Java_com_example_autolight_1android_customize_1standard_CustomizeStandardActivit
             double real_facesize_y = faces[i].y / resizeRatio;
             double real_facesize_width = faces[i].width / resizeRatio;
             double real_facesize_height = faces[i].height / resizeRatio;
+            double real_facesize_area = real_facesize_width * real_facesize_height;
+
+            if (real_facesize_area < 10000) { return -1; }
 
             Point center(real_facesize_x + real_facesize_width / 2,
                          real_facesize_y + real_facesize_height / 2);
             ellipse(img_result, center, Size(real_facesize_width / 2, real_facesize_height / 2), 0,
                     0, 360,
-                    Scalar(255, 0, 255), 30, 8, 0);
+                   Scalar(255, 0, 255), 3, 8, 0);
 
 
-            Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,
-                           real_facesize_height);
-            rectangle(img_input, Rect(real_facesize_x, real_facesize_y, real_facesize_width,
-                                      real_facesize_height), Scalar(0, 0, 255), 2, 8, 0);
-            Mat faceROI = img_gray(face_area);
+           Mat faceROI(img_gray, Rect(real_facesize_x, real_facesize_y, real_facesize_width,
+                           real_facesize_height));
+             //rectangle(img_result, Rect(real_facesize_x, real_facesize_y, real_facesize_width,
+                                 //     real_facesize_height), Scalar(255, 0, 255), 2, 8, 0);
 
             int meanOutput;
             meanOutput = (int) mean(faceROI)[0];
